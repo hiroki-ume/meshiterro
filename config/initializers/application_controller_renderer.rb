bbb20075967a58112579e89f05530ca91ae128b6
hiroki-ume <h.ume1219bknb@gmail.com>
2020-03-03 18:05:25 +0900

    [Start]meshiterro

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '5684ac714a2f66775f9f856c77d0f39cfa439369a3bd02feb73009194b16788a8112c4c1167f53b88ada7c8b00ff4583f70b72c2cda1aad308779d718bb3a079'
